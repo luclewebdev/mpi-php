dfb1f61388e3856fabe5444b89b448cb86cce742
luclewebdev <75719880+luclewebdev@users.noreply.github.com>
2022-09-05 17:03:59 +0200

    Début Model message

---
+++ core/Models/Message.php
@@ -0,0 +1,20 @@
+<?php
+
+
+// recuperer le fichier contenant pdo
+
+class Message
+{
+
+
+    // le stocker dans une propriété protegee
+
+    // instancier cette propriété par son constructeur
+
+
+    // field par colonne sql + getters et setters
+
+    //a partir de la, on peut regler PDO a la connection en mode FETCH_OBJ
+
+
+}
